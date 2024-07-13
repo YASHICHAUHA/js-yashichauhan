67d4d6d3e6b87d03818e28c076a836cab9ec88ab
YASHI CHAUHAN <97442087+YASHICHAUHA@users.noreply.github.com>
2024-07-13 03:39:23 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID=144553
+let accountemail="yashichauhan@gmail.com"
+var accountpassword="yashi"
+accountCity="jaipur"
+let accountstate
+// accountID=2 
+console.log(accountID);
+accountemail="yassaschauhan21@gmail.com"
+accountpassword="yasss"
+console.table([accountID,accountemail,accountpassword,accountCity,accountstate]);
+/* prefer not to use var
+beacause of issues in block scope  and functional scope*/
