e852f90f856077da18d9b7a51e8d1d2b90014e2d
YASHI CHAUHAN <97442087+YASHICHAUHA@users.noreply.github.com>
2024-07-13 07:12:53 +0000

    conversionoperationss

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -14,6 +14,29 @@ console.log(booleanIsLoggedIn);
 //1=>true
 //""=>false
 //"yashi"=>true
-let somenumber=33
-let stringsomenumber=String(somenumber)
-console.log(somenumber)
+//let somenumber=33
+//let stringsomenumber=String(somenumber)
+//console.log(somenumber)
+//******************operations***************************//
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+//let str1="yashi"
+//let str2="chauhan"
+//let str3=str1+str2
+//console.log(str3);
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log((3+4)* 5 % 3);
+console.log(+true);
+console.log(+"")
+let gamecounter=100
+++gamecounter
+console.log(gamecounter);
+// link to study for prefix and suffix in js in mdn
