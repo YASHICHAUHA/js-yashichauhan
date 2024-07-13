e114b9af3e04c5cbd5bc8b1295bc92e0bbbe6ca4
YASHI CHAUHAN <97442087+YASHICHAUHA@users.noreply.github.com>
2024-07-13 06:05:39 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,19 @@
+//let score="33 abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber= Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber);
+//"33"=>33
+//"33abc"=>NAN
+//true=>1;
+//false=>0;
+let isLoggedIn = "yashi"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true
+//""=>false
+//"yashi"=>true
+let somenumber=33
+let stringsomenumber=String(somenumber)
+console.log(somenumber)
