8ee20b259c86d0fc8cc9004eeb378ff637f40d4d
YASHI CHAUHAN <97442087+YASHICHAUHA@users.noreply.github.com>
2024-07-15 05:30:12 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -23,3 +23,19 @@ const myfunction=function(){
 }
 //how  to detect the data types of any value
 console.log(typeof outsidetemperature)
+//****************************************************** */
+//there are two types of memory  stack and heap 
+//where stack and heap used stack always used in primitive data types and heap always used in non-premitive dataq types
+let myyoutubename="yashichauhan.com"
+let anothername=myyoutubename
+anothername="chaiorcode"
+console.log(myyoutubename)
+console.log(anothername)
+let userone={
+    email:"user@google.com",
+    upi:"user@byl"
+}
+let usertwo=userone
+usertwo.email="yashi@google.com"
+console.log(userone.email);
+console.log(usertwo.email);
