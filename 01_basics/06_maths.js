9f4c2af6d0d4849f3d77c587165d253b37093240
YASHI CHAUHAN <97442087+YASHICHAUHA@users.noreply.github.com>
2024-07-24 01:36:59 +0000

    maths.js

---
+++ 01_basics/06_maths.js
@@ -0,0 +1,20 @@
+const score=400
+const balance =new Number(100)
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+const othernumber=123.8966
+//console.log(othernumber.toPrecision(3));
+const hundreds=10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+//******************MATHS******************* */
+console.log(Math);
+console.log(Math.abs(-4));// conver negative into positive 
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2))
+console.log(Math.min(4,3,6,8));
+console.log(Math.min(3,4,5,6,33))
+console.log(Math.floor(Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
