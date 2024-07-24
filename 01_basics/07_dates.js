dddf89b51586ee8d52d6f3f03004005ba5c50b35
YASHI CHAUHAN <97442087+YASHICHAUHA@users.noreply.github.com>
2024-07-24 02:21:39 +0000

    dates.js

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,24 @@
+// dates
+let myDate=new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+let myCreateDate=new Date("01-14-2023")
+let myTimeStamp=Date.now();
+//console.log(myTimeStamp);
+//console.log(myCreateDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+let mydate=new Date();
+//console.log(mydate);
+//console.log(mydate.getMonth()+1);
+//console.log(mydate.getDay());
+mydate.toLocaleString('default',{weekday:"long"})
+
+
+
+
+
+
+
+
